1826d4c19189daf70a033f98f8d54f0284b3cddd
Armando Miranda <mirandaarmando@hotmail.com>
2026-07-24 11:24:54 -0500

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,20 @@
+(function () {
+    const navbar = document.querySelector("#navbar");
+    const hamburger = document.querySelector("#hamburger");
+
+    hamburger.addEventListener("click", function () {
+        navbar.classList.toggle("open");
+    });
+})();
+
+const formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Formulario enviado");
+});
+// const form = document.querySelector("form");
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     alert("Formulario enviado");
+// });
